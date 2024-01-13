35589898d7dadb7da5e6d5e206b08418be865886
piainmaster <90442137+piainmaster@users.noreply.github.com>
2024-01-13 18:59:02 +0100

    rollback dwconv change

--- models/convnext.py
+++ models/convnext.py
@@ -23,27 +23,29 @@ class Block(nn.Module):
         drop_path (float): Stochastic depth rate. Default: 0.0
         layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
     """
-    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+     def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
         super().__init__()
-        # Replace depthwise convolution with standard 3x3 convolution
-        self.conv1 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, bias=False)
-        self.norm1 = LayerNorm(dim, eps=1e-6)
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
         self.act = nn.GELU()
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, bias=False)
-        self.norm2 = LayerNorm(dim, eps=1e-6)
+        self.pwconv2 = nn.Linear(4 * dim, dim)
         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
                                     requires_grad=True) if layer_scale_init_value > 0 else None
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
         input = x
-        x = self.conv1(x)
-        x = self.norm1(x)
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
         x = self.act(x)
-        x = self.conv2(x)
-        x = self.norm2(x)
+        x = self.pwconv2(x)
         if self.gamma is not None:
             x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
         x = input + self.drop_path(x)
         return x
 
